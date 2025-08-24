23e469fd96f29cd8e93268ccd0a54b4650820d27
manu-paul20 <manupaul535@gmail.com>
2025-08-24 09:54:29 +0530

    Added login button functionality

--- src/Login.java
+++ src/Login.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.ResultSet;
 
 public class Login extends JFrame implements ActionListener {
     JTextField uid_input = new JTextField();
@@ -34,13 +35,13 @@ public class Login extends JFrame implements ActionListener {
         uid_input.setForeground(Color.red);
         add(uid_input);
 
-        JLabel password_text = new JLabel("Enter User ID : ");
+        JLabel password_text = new JLabel("Enter password : ");
         password_text.setBounds(40,200,300,20);
         password_text.setFont(new Font("",Font.BOLD,20));
         add(password_text);
 
         //password input
-        password_input.setBounds(190,200,300,30);
+        password_input.setBounds(220,200,300,30);
         password_input.setFont(new Font("",Font.BOLD,20));
         password_input.setSelectedTextColor(Color.BLUE);
         password_input.setForeground(Color.red);
@@ -51,6 +52,7 @@ public class Login extends JFrame implements ActionListener {
         login_button.setCursor(new Cursor(Cursor.HAND_CURSOR));
         login_button.setBackground(Color.BLUE);
         login_button.setForeground(Color.WHITE);
+        login_button.addActionListener(this);
         login_button.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseExited(MouseEvent e) {
@@ -65,11 +67,11 @@ public class Login extends JFrame implements ActionListener {
             public void mouseEntered(MouseEvent e) {
                 login_button.setBackground(Color.WHITE);
                 login_button.setBorder(BorderFactory.createLineBorder(Color.BLUE,5));
+                login_button.setForeground(Color.black);
                 if(uid_input.getText().isBlank() && password_input.getText().isBlank()){
                     login_button.setBounds(200,300,150,50);
                     login_button.setText("Fill all fields");
                     login_button.setEnabled(false);
-
                 }
             }
         });
@@ -80,6 +82,7 @@ public class Login extends JFrame implements ActionListener {
         signup_button.setCursor(new Cursor(Cursor.HAND_CURSOR));
         signup_button.setBackground(Color.blue);
         signup_button.setForeground(Color.white);
+        signup_button.addActionListener(this);
         signup_button.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
@@ -89,7 +92,7 @@ public class Login extends JFrame implements ActionListener {
                     signup_button.setBounds(500,300,150,50);
                     signup_button.setText("Fill all fields");
                     signup_button.setEnabled(false);
-
+                    signup_button.setForeground(Color.black);
                 }
             }
 
@@ -111,11 +114,46 @@ public class Login extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e){
         if(e.getSource()==login_button){
-
+            String user_id = uid_input.getText().toLowerCase();
+            String password = password_input.getText();
+            String result = searchUser(user_id,password);
+            if(result.equals("NOT_FOUND")){
+                JOptionPane.showMessageDialog(
+                        null,
+                        "User not found\nPlease check details",
+                        "ERROR",
+                        JOptionPane.INFORMATION_MESSAGE
+                );
+            }else{
+                String message = "Welcome "+result;
+                JOptionPane.showMessageDialog(null,message);
+            }
         }
     }
 
     public static void main(String[] args) {
      new Login();
+    }
+    public String searchUser(String user_id,String password){
+        String query = "SELECT * FROM users WHERE user_id = '"+user_id+"'";
+        DB_CONNECTION connection = new DB_CONNECTION();
+        try{
+            ResultSet resultSet = connection.statement.executeQuery(query);
+            if(resultSet.next()){
+                String user_pass = resultSet.getString("password");
+                String user_name = resultSet.getString("user_name");
+                resultSet.close();
+                System.out.println("Connection closed");;
+                return (user_pass.equals(password)) ? user_name : "NOT_FOUND";
+            }
+            resultSet.close();
+            connection.connection.close();
+            System.out.println("Connection closed");
+            return "NOT_FOUND";
+        }catch (Exception e){
+            System.out.println(e);
+            return "ERROR";
+        }
+
     }
 }
