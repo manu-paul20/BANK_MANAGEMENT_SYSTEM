a7a0b5516b788ab115cc5c7512de25be5bdd881c
manu-paul20 <manupaul535@gmail.com>
2025-08-24 09:55:10 +0530

    Created DB connection

--- src/DB_CONNECTION.java
+++ src/DB_CONNECTION.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DB_CONNECTION {
+    Connection connection;
+    Statement statement;
+    DB_CONNECTION(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql:///bank_db", "root", "Durgapur@1");
+            statement = connection.createStatement();
+            System.out.println("Database connected successfully");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
 }
